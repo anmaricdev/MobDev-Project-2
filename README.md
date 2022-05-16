0879a8485bc45f63a449819a74bd1af447805221
Ante Maric <antemaric99@hotmail.com>
2022-05-16 10:50:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MobDev-Project-2
+ An Android app that let's you play the popular classic game "three in a row" with another player
---
+++ app/src/main/java/tic/tac/toe/MainActivity.kt
@@ -0,0 +1,11 @@
+package tic.tac.toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
