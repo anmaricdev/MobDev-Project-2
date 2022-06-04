7f596198550de92564ee23ad26f5d73217d4eaff
Ante Maric <49611582+anmaricdev@users.noreply.github.com>
2022-06-05 00:51:25 +0200

    added splash screen
    added a splash screen that shows for 3s before transitioning to the app

--- app/src/main/java/tic/tac/toe/MainActivity.kt
+++ app/src/main/java/tic/tac/toe/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //hides the top bar that says "Three in a row"
+        //supportActionBar?.hide()
+
     }
 }
---
+++ app/src/main/java/tic/tac/toe/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package tic.tac.toe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //hides the top bar that says "Three in a row"
+        supportActionBar?.hide()
+
+        //after 3 seconds of splash screen action, switch from this splash screen activity back to
+       Handler(Looper.getMainLooper()).postDelayed({
+           val intent = Intent (this@SplashScreenActivity, MainActivity::class.java)
+           startActivity(intent)
+           finish() //prevents the app returning to this screen if e.g. back button is pressed after switching to main app
+        }, 3000)
+
+    }
+}
